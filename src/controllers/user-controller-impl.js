8a77ae59509b97fa7d6284967eea77ced35eccf9
Umar Ma'ruf Mutaqin <umarmarufmutaqin@gmail.com>
2023-07-20 11:51:55 +0700

    add crud

--- src/controllers/user-controller-impl.js
+++ src/controllers/user-controller-impl.js
@@ -1,27 +1,196 @@
 const config = require('../database/database');
 const mysql = require('mysql');
 const pool = mysql.createPool(config);
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 
 pool.on('error', (err) => {
     console.error(err);
 });
 
+const users = [];
+app.use(bodyParser.json());
+
 module.exports = {
-    // Ambil data semua karyawan
-    getUser(req, res) {
-        const query = 'SELECT * FROM users LIMIT 2';
-        console.log(query); // Mencetak query ke konsol
+    // Get all users
+    FindAll(req, res) {
+        const query = 'SELECT * FROM users LIMIT 500';
 
         pool.query(query, (error, results) => {
+            console.log(query);
             if (error) {
-                throw error;
+                console.error('Error fetching users:', error);
+                res.status(500).json({
+                    success: false,
+                    message: "Internal Server Error"
+                });
+            } else {
+                const message = results.length === 0 ? "Record not found" : "Record found";
+                res.status(200).json({
+                    success: true,
+                    message: message,
+                    data: results
+                });
             }
-            res.send({
-                success: true,
-                message: 'Record Found',
-                data: results
+        });
+    },
+
+    // Get user by ID
+    FindByID(req, res) {
+        const userId = req.params.id;
+        const query = `SELECT * FROM users WHERE id = ? LIMIT 1`;
+
+        pool.query(query, [userId], (error, results) => {
+            console.log(results);
+            if (error) {
+                console.error('Error this query:', error);
+                res.status(500).json({
+                    success: false,
+                    message: "Internal Server Error"
+                });
+            } else {
+                if (results.affectedRows === 0) {
+                    res.status(404).json({
+                        success: false,
+                        message: "User not found",
+                        data: results
+                    });
+                } else {
+                    res.status(200).json({
+                        success: true,
+                        message: "Record found",
+                        data: results
+                    });
+                }
+            }
+        });
+    },
+
+
+    // Create a new user
+    Create(req, res) {
+        if (!req.body.name) {
+            res.status(400).json({
+                message: "Bad Request: 'name' field is required"
             });
+        } else {
+            const newUser = {
+                name: req.body.name
+            };
+            users.push(newUser);
+            if (users.length > 0) {
+                const name = users[users.length - 1].name;
+                const query = `INSERT INTO users (name) VALUES (?)`;
+
+                pool.query(query, [name], (error, results) => {
+                    console.log(results);
+                    if (error) {
+                        console.error('Error creating user:', error);
+                        if (error.code === 'ER_DUP_ENTRY') {
+                            res.status(500).json({
+                                success: false,
+                                message: "User with the provided name already exists"
+                            });
+                        } else {
+                            res.status(500).json({
+                                success: false,
+                                message: "Internal Server Error"
+                            });
+                        }
+                    } else {
+                        res.status(201).json({
+                            success: true,
+                            message: 'User created successfully',
+                            data: newUser
+                        });
+                    }
+                });
+            } else {
+                res.status(500).json({
+                    success: false,
+                    message: 'Internal Server Error'
+                });
+            }
+        }
+    },
+
+    // Update user data
+    Update(req, res) {
+        const userId = req.params.id;
+        const newName = req.body.name;
+
+        if (!newName) {
+            res.status(400).json({
+                message: "Bad Request: 'name' field is required"
+            });
+            return;
+        }
+
+        const query = `UPDATE users SET name = ? WHERE id = ?`;
+        const updateUser = {
+            id: userId,
+            name: newName
+        };
+
+        pool.query(query, [newName, userId], (error, results) => {
+            console.log(results);
+            if (error) {
+                console.error('Error updating user:', error);
+                if (error.code === 'ER_DUP_ENTRY') {
+                    res.status(500).json({
+                        success: false,
+                        message: "User with the provided name already exists"
+                    });
+                } else {
+                    res.status(500).json({
+                        success: false,
+                        message: "Internal Server Error"
+                    });
+                }
+            } else {
+                if (results.affectedRows === 0) {
+                    res.status(404).json({
+                        success: false,
+                        message: "User not found",
+                    });
+                } else {
+                    res.status(200).json({
+                        success: true,
+                        message: "User updated successfully",
+                        data: updateUser
+                    });
+                }
+            }
+        });
+    },
+
+    // Delete user
+    Delete(req, res) {
+        const userId = req.params.id;
+
+        const query = `DELETE FROM users WHERE id = ?`;
+
+        pool.query(query, [userId], (error, results) => {
+            if (error) {
+                console.error('Error deleting user:', error);
+                res.status(500).json({
+                    success: false,
+                    message: "Internal Server Error"
+                });
+            } else {
+                if (results.affectedRows === 0) {
+                    res.status(404).json({
+                        success: false,
+                        message: "User not found",
+                    });
+                } else {
+                    res.status(200).json({
+                        success: true,
+                        message: "User deleted successfully",
+                    });
+                }
+            }
         });
     }
-
-}
+};
--- src/routes/user.js
+++ src/routes/user.js
@@ -3,14 +3,16 @@ const {
     user
 } = require('../controllers/user-controller');
 
-// GET localhost:8080/karyawan => Ambil data semua karyawan
-router.get('/users', user.getUser);
+router.get('/users', user.FindAll);
+router.get('/users/:id', user.FindByID);
+router.post('/users', user.Create);
+router.put('/users/:id', user.Update);
+router.delete('/users/:id', user.Delete);
 
 router.stack.forEach((route) => {
     if (route.route && route.route.path) {
         console.log(`${route.route.stack[0].method.toUpperCase()} ${route.route.path}`);
     }
 });
-
 
 module.exports = router;
